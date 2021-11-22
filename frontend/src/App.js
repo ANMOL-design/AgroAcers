ca384bd6c3f6277e82daff7ace312b6380ceb802
ANMOL-design <65813590+ANMOL-design@users.noreply.github.com>
2021-11-22 23:50:32 +0530

    Update The Footer and add PageNotFound

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,31 @@
 import React from 'react';
 import "./Styles/index.css"
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {BrowserRouter, Route,Routes}  from 'react-router-dom'
 import Navbar from "./component/header/Navbar";
 import Home from "./component/home/Home";
 import Login from "./component/auth/login/Login";
-import Register from "./component/auth/Registration/Register"; <<
-<< << < HEAD
-import PageNotFound from './component/PageNotFound/PageNotFound'; ===
-=== =
-import Footer from './component/footer/footer'; >>>
->>> > d94b8fdf895bdca9c1b394350a2090f4c1f135c9
+import Register from "./component/auth/Registration/Register";
+import PageNotFound from './component/PageNotFound/PageNotFound';
+import Footer from './component/footer/footer';
 
 class App extends React.Component {
-    render() {
-        return ( <
-            >
-            <
-            BrowserRouter >
-            <
-            Navbar / > {
-                /* <Routes>
-                    <Route  path="/login" component ={Login} ></Route>
-                    <Route  path="/register" component ={Register} ></Route>
-                    <Route exact  path="/" component ={Home} ></Route>
-                      </Routes> */
-            } <
-            Routes >
+  render(){
+  return (
+   <>
+      <BrowserRouter>
+          <Navbar/>
+          <Routes>
+                <Route path="login" element={<Login/>}/>
+                <Route path="register" element={<Register/>}/>
+                <Route path="register/login" element={<Login/>}/>
+                <Route path="/" element={<Home/>} exact/>
+                <Route path="*" element={<PageNotFound/>}/>
+            </Routes>
+            <Footer />
+       </BrowserRouter>
+   </>
 
-            <
-            Route path = "login"
-            element = { < Login / > }
-            /> <
-            Route path = "register"
-            element = { < Register / > }
-            /> <
-            Route path = "register/login"
-            element = { < Login / > }
-            /> <
-            Route path = "/"
-            element = { < Home / > }
-            exact / >
-            <
-            Route path = "*"
-            element = { < PageNotFound / > }
-            />
-
-            <
-            /Routes> <
-            Footer / >
-            <
-            /BrowserRouter> <
-            />
-
-        );
+  );
     }
 }
 
